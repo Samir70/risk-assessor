ac2f51be484c3572c6bdda13a9ffb6d090a2009c
Samir70 <sbetmouni@talktalk.net>
2019-08-01 09:41:09 +0100

    test ability to communicate thru store

--- src/Components/Forms/WhoAtRiskForm.js
+++ src/Components/Forms/WhoAtRiskForm.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Form, Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 
@@ -6,8 +7,28 @@ const WhoAtRiskForm = (props) => {
     const personAtRiskInput = React.createRef();
     const handleSubmit = (event) => {
         event.preventDefault();
+        var personAtRisk = personAtRiskInput.current.value.trim();
+        if (props.peopleAtRisk.includes(personAtRisk)) {
+            alert('The list of people at risk already includes '+personAtRisk)
+        } else {
+            if (personAtRisk) {
+                props.dispatch({type:'ADD_PERSON_AT_RISK', person:personAtRisk});
+                personAtRiskInput.current.value = '';
+            }
+        }
     }
-    console.log('WhoAtRiskForm')
+
+    const updateFormText = (people) => {
+        if (people.length===0) {return 'So far: no-one is at risk.'}
+        if (people.length===1) {return people[0]+' is at risk.'}
+        return people.slice(0, -1).join(', ')+' and '+people.slice(-1)+' are at risk.'
+    }
+
+    props.dispatch({type:'UPDATE_AT_RISK_TEXT', newText: updateFormText(props.peopleAtRisk)})
+
+    const peopleList = props.peopleAtRisk.map((person, i) => <li key={i}>{person}</li>);
+
+    console.log('WhoAtRiskForm');
 
     return (
         <div>
@@ -20,8 +41,17 @@ const WhoAtRiskForm = (props) => {
                 </Form.Group>
                 <Button variant="primary" type="submit">Add Person At Risk</Button>
             </Form>
+            <hr />
+            <h3>People at Risk</h3>
+            {props.peopleAtRisk.length === 0 ? <p>No-one currently listed</p> : <ul>{peopleList}</ul>}
         </div>
     )
 }
 
-export default WhoAtRiskForm;
+const mapStateToProps = (state) => {
+    return {
+        peopleAtRisk: state.peopleAtRisk
+    }
+}
+
+export default connect(mapStateToProps)(WhoAtRiskForm);
--- src/Reducers/rootReducer.js
+++ src/Reducers/rootReducer.js
@@ -1,6 +1,7 @@
 const initialState = {
     sectionTitle: 'Pick a section to get started', 
     formID: 'none', // use this to select the formText
+    // formTexts are the text summaries that appear in clipboard area once data is added
     formTexts: {
         form0: 'text for form0',
         form1: 'text for form1',
@@ -8,7 +9,9 @@ const initialState = {
         form3: 'text for form3',
         form4: 'text for form4',
         form5: 'text for form5'
-    }    
+    },  
+    
+    peopleAtRisk: []
 }
 
 const rootReducer = (state = initialState, action) => {
@@ -18,6 +21,15 @@ const rootReducer = (state = initialState, action) => {
             ...state,
             sectionTitle: action.newSection, 
             formID: action.formID
+        }
+
+        case 'ADD_PERSON_AT_RISK' : return {
+            ...state,
+            peopleAtRisk: state.peopleAtRisk.concat(action.person)
+        }
+        case 'UPDATE_AT_RISK_TEXT' : return {
+            ...state,
+            formTexts: {...state.formTexts, form0:action.newText}
         }
         default: return state;
     }    
