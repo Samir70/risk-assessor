44ae49a81ce9868580cb21d9ed1fe5d65edd0f13
Samir70 <sbetmouni@talktalk.net>
2019-08-01 07:50:32 +0100

    prevent default on submit WhoAtRiskForm

--- src/Components/Forms/WhoAtRiskForm.js
+++ src/Components/Forms/WhoAtRiskForm.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Form, Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
 
 const WhoAtRiskForm = (props) => {
     const personAtRiskInput = React.createRef();
     const handleSubmit = (event) => {
-
+        event.preventDefault();
     }
+    console.log('WhoAtRiskForm')
 
     return (
         <div>
