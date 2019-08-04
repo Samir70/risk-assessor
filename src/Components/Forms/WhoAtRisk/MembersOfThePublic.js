ecac2a0577970eb5557a2467cad5753efe29da4d
Samir70 <sbetmouni@talktalk.net>
2019-08-04 11:45:32 +0100

    enable two way link between state and checkboxes

--- src/Components/Forms/WhoAtRisk/MembersOfThePublic.js
+++ src/Components/Forms/WhoAtRisk/MembersOfThePublic.js
@@ -5,7 +5,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 import { CHECK_MALE, CHECK_FEMALE, CHECK_TRANS } from '../../../Reducers/actions';
 
-const MembersOfThePublic = ({maleChecked, dispatch}) => {
+const MembersOfThePublic = ({maleChecked, femaleChecked, transChecked, dispatch}) => {
     return (
         <div>
             <h3>Members of the public</h3>
@@ -17,12 +17,15 @@ const MembersOfThePublic = ({maleChecked, dispatch}) => {
                   <Col sm={9}>
                       <Form.Check inline type='checkbox' 
                         label='Male' 
+                        checked={maleChecked}
                         onChange={()=>dispatch({type:CHECK_MALE})} />
                       <Form.Check inline type='checkbox' 
                         label='Female'
+                        checked={femaleChecked}
                         onChange={()=>dispatch({type:CHECK_FEMALE})} />
                       <Form.Check inline type='checkbox' 
                         label='Transgender'
+                        checked={transChecked}
                         onChange={()=>dispatch({type:CHECK_TRANS})} />
                   </Col>
               </Form.Group>
@@ -33,7 +36,9 @@ const MembersOfThePublic = ({maleChecked, dispatch}) => {
 
 const mapStateToProps = (state) => {
     return {
-        maleChecked: state.WhoAtRisk.memPub.male
+        maleChecked: state.WhoAtRisk.memPub.male,
+        femaleChecked: state.WhoAtRisk.memPub.female,
+        transChecked: state.WhoAtRisk.memPub.transgender
     }
 }
 
